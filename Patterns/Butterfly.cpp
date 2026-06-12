643e0638777d9d84d35b3362d3a5320b3dc797b0
Syeda Marium Fatima <syedamariumfatima20@gmail.com>
2026-06-12 21:27:11 +0500

    Create Butterfly.cpp

---
+++ Patterns/Butterfly.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=4;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++) cout<<"*";
+        for(int k=0; k<n+2*(1-i); k++) cout<<" ";
+        for(int j=0; j<i+1; j++) cout<<"*";
+        cout<<endl;
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++) cout<<"*";
+        for(int k=0; k<2*i; k++) cout<<" ";
+        for(int j=0; j<n-i; j++) cout<<"*";
+        cout<<endl;
+    }
+    return 0;
+}
